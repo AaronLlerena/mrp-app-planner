eee761c05447d2bbae552572b6728f0cd9ec58c8
Aaron Llerena Arroyo <aaron.llerena@unmsm.edu.pe>
2025-12-16 01:37:30 +0000

    fix: added missing vite config and index html to root

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CRUCIAL: Necesario para que Vercel sepa dónde iniciar
   base: '/', 
   build: {
     outDir: 'dist', 
